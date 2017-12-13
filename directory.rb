5669c0a99f961451569703ba38ead150280ddb29
Atty Cronin <antoniajcronin@gmail.com>
2017-12-13 12:15:13 +0000

    added open/close file and refactored with instance variables

--- directory.rb
+++ directory.rb
@@ -1,81 +1,57 @@
 @students = []
+@filename
+@name
+@@cohort
 @months = [:january, :february, :march, :april, :may, :june, 
   :july, :august, :september, :october, :november, :december]
 #Let's put all the @students into an array
 
 def input_students
-
-  # def get_input(input, prompt)
-  #   while input.empty? do 
-  #     puts prompt
-  #     input = gets.chomp
-  #   end
-  #   input
-  # end
-
-  #get student name
-  puts "Please enter the STUDENT NAME"
-  name = gets.chomp
-  while name.empty? 
-    # get_input(name, "Please enter the STUDENT NAME!") 
-    puts "Please enter the STUDENT NAME"
-    name = gets.chomp
+  get_student_name
+  while @name.empty? 
+    get_student_name
   end
- 
-  #get cohort
-  puts "Enter the COHORT (November or December)"
-  cohort = gets.chomp.downcase.to_sym
-  while cohort.empty? 
-    #get_input(cohort, "Enter the COHORT (November or December)") 
-    puts "Enter the COHORT (November or December)"
-    cohort = gets.chomp.downcase.to_sym
+  get_cohort
+  while @cohort.empty? 
+    get_cohort 
   end
 
-  #not working with instance variable
-  # if !cohort.to_s.include?(@months.to_s)
-  #   puts "Enter the COHORT (November or December!!)" 
-  #   cohort = gets.chomp.downcase.to_sym
-  # end
-  
   puts("To finish, just hit return twice")
-  puts "#{name}, #{cohort}"
+  puts "#{@name}, #{@cohort}"
 
-  #while the name and cohort are not empty, repeat this code
-  while !name.empty? && !cohort.empty? do
+  #while the @name and @cohort are not empty, repeat this code
+  while !@name.empty? && !@cohort.empty? do
     #add the student hash to the array
-    @students << {name: name, cohort: cohort}
+    students_to_array(@name, @cohort)
     puts "Now we have #{@students.count} #{@students.count !=1 ? "students" : "student"}"
-    
-    #get student name
-    puts "Please enter the STUDENT NAME!"
-    name = gets.chomp.capitalize
-  
-    #get cohort
-    puts "Enter the COHORT (November or December)"
-    cohort = gets.chomp.downcase
-  end
+    get_student_name
+    get_cohort end
   #return the array of @students
   @students
 end
- 
+
+def students_to_array(name, cohort)
+  @students << {name: name, cohort: cohort}
+end
  
 def print_menu
+  menu_items = [
+    "1. Input the students", 
+    "2. Show the students", 
+    "3. Save the list to students.csv", 
+    "4. Load the list from students.csv",
+    "9. Exit" ]
   # 1. Print the menu and ask the user what to do
-  puts "1. Input the @students"
-  puts "2. Show the @students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv"
-  puts "9. Exit" 
+  menu_items.map {|m| puts m}
 end
 
 def print_header
-  puts "The students of Villains' Academy"
-  puts "-------------"
+  puts "The students of Villains' Academy \n-------------"
 end
 
 def print_students_list
-  @months.each  do |month|
-    @students.map { |student| puts "#{student[:name]},  #{student[:cohort].to_s.capitalize} cohort".center(20) if student[:cohort].to_s.include?(month.to_s)}
+  @months.each do |month|
+    @students.map { |student| puts "#{student[:name]},  #{student[:cohort].to_s.capitalize} cohort".center(20) if student[:cohort].to_s.capitalize.include?(month.to_s)}
   end
 end
 
@@ -90,17 +66,18 @@ def show_students
   print_footer
 end
 
-def process(selection)
+def menu(selection)
   case selection
     when "1"
       @students = input_students
     when "2"
       show_students
     when "3"
-      save_students
+      get_filename
+      save_students(@filename)
     when "4"
-      load_students
-        
+      get_filename
+      load_students(@filename)       
     when "9"
       exit # this will cause the program to terminate
     else
@@ -108,36 +85,64 @@ def process(selection)
     end
 end
 
-def interactive_menu
+def get_filename
+    puts "Enter filename"
+    @filename = STDIN.gets.chomp
+end
+def get_student_name
+  puts "Please enter the STUDENT name"
+  @name = STDIN.gets.chomp.capitalize
+end
+def get_cohort 
+  puts "Enter the cohort (November or December)"
+  @cohort = STDIN.gets.chomp.downcase.to_sym
+end
 
+def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    menu(STDIN.gets.chomp)
   end
 end 
 
-def save_students
+def save_students(filename)
   #open file for writing
-  file = File.open("students.csv", "w")
+  file = File.open(@filename, "w")
   #iterate over the array of students
   @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
+    student_data = [student[:@name], student[:@cohort]]
     csv_line = student_data.join(",")
     file.puts csv_line
   end
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(@filename, "r")
   file.readlines.each do |line|
-    name, cohort = line.chomp.split(",")
-    @students << {name: name, cohort: cohort.to_sym}
+    @name, @cohort = line.chomp.split(",")
+    students_to_array(@name, @cohort)
   end
   file.close
 end
 
+def try_load_students(filename = "students.csv")
+  puts "Loading #{@filename}..."
+  #@filename = ARGV.first # first arg from command line
+
+  return if @filename.nil? # exit if no @filename argument
+  if File.exists?(@filename)
+    load_students(@filename)
+    show_students
+    puts "Loaded #{@students.count} from #{@filename}"
+  else # if it doesn't exist
+    puts "Sorry, #{@filename} doesn't exist"
+    exit # quit program
+  end
+end
+
 #nothing happens until we call the methods
+try_load_students
 interactive_menu
 
 @students = input_students
